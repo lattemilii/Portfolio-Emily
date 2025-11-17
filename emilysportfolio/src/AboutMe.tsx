84953074346d1d2ebb3edabaa5789c6c7063958d
Emily Francesca Gouw <emilygouw4@gmail.com>
2025-11-17 13:38:21 +0700

    Update AboutMe.tsx

--- emilysportfolio/src/AboutMe.tsx
+++ emilysportfolio/src/AboutMe.tsx
@@ -2,8 +2,8 @@ function AboutMe() {
     return(
         <>
         <section id="about" className="flex flex-col md:flex-row items-center justify-center min-h-screen sm:px-6 md:px-24 overflow-x-hidden md:gap-12">
-            <div className="md:w-auto max-w-sm mt-20 md:mt-0 md:max-w-md flex-1 flex justify-center">
-                <img src="/Emily-1.png" alt="Emily" className="md:w-auto md:h-auto h-96" />
+            <div className="md:w-auto max-w-sm mt-28 md:mt-0 md:max-w-md flex-1 flex justify-center">
+                <img src="/Emily-1.png" alt="Emily" className="md:w-auto md:h-auto h-70" />
             </div>
             <div className="w-full max-w-2xl flex-1">
                 <h1 className="text-2xl md:text-6xl font-bold mb-4 text-primary text-center md:text-left whitespace-normal md:px-0 sm:px-6">About Emily</h1>
